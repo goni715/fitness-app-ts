0855f3e8bab817946711395473ae1fbda1ba3224
goni715 <halesgoni715149123@gmail.com>
2024-12-21 15:30:23 +0600

    google font added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import About from "./components/About";
 
 const App = () => {
   return (
     <>
-      <h1>Home Page</h1>
-      <About name="Osman Goni" />
+      <div className="app">
+
+      </div>
     </>
   );
 };
