d578ca4d32473d26d845644a1691ada48b135b5f
goni715 <halesgoni715149123@gmail.com>
2024-12-23 23:55:48 +0600

    home component

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -5,6 +5,8 @@ import SponsorRedBull from "../../assets/SponsorRedBull.png";
 import SponsorForbes from "../../assets/SponsorForbes.png";
 import SponsorFortune from "../../assets/SponsorFortune.png";
 import useMediaQuery from "../../hooks/useMediaQuery";
+import ActionButton from "../../shared/ActionButton";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 
 type TProps = {
@@ -17,26 +19,66 @@ const Home = ({ setSelectedPage }: TProps) => {
 
 
     return (
-        <>
-           <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
-             {/* IMAGE AND MAIN HEADER */}
-             <div>
-                {/* Main Header */}
-                <div>
-                    {/* Headings */}
-                    <div>
-                        <div>
-                            <div>
-                                <img src={HomePageText} alt="home-page-text" />
-                            </div>
-                        </div>
-                    </div>
+      <>
+        <section
+          id="home"
+          className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0"
+        >
+          {/* IMAGE AND MAIN HEADER */}
+          <div className="w-5/6 mx-auto md:flex items-center justify-center md:h-5/6">
+            {/* Main Header */}
+            <div className="z-10 mt-32 md:basis-3/5">
+              {/* Headings */}
+              <div className="md:-mt-20">
+                <div className="relative">
+                  <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext">
+                    <img src={HomePageText} alt="home-page-text" />
+                  </div>
                 </div>
+                <p className="mt-8 text-sm">
+                  Unrivaled Gym. Unparalleled Training Fitness Classes. World
+                  Class Studios to get the Body Shapes That you Dream of.. Get
+                  Your Dream Body Now.
+                </p>
+              </div>
+              {/* Actions */}
+              <div className="mt-8 flex items-center gap-8">
+                <ActionButton setSelectedPage={setSelectedPage}>
+                  Join Now
+                </ActionButton>
+                <AnchorLink
+                  className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
+                  onClick={() => setSelectedPage(TSelectedPage.ContactUs)}
+                  href={`#${TSelectedPage.ContactUs}`}
+                >
+                  <p>Learn More</p>
+                </AnchorLink>
+              </div>
+            </div>
 
-                {/* Image */}
-             </div>
-           </section>
-        </>
+            {/* Image */}
+            <div
+              className="flex basis-3/5 justify-center md:z-10
+              md:ml-40 md:mt-16 md:justify-items-end"
+            >
+              <img alt="home-pageGraphic" src={HomePageGraphic} />
+            </div>
+          </div>
+
+          {/* SPONSORS */}
+          {isAboveMediumScreens && (
+            <div className="h-[150px] w-full bg-primary-100 py-10">
+              <div className="mx-auto w-5/6">
+                <div className="flex w-3/5 items-center justify-between gap-8">
+                  <img alt="redbull-sponsor" src={SponsorRedBull} />
+                  <img alt="forbes-sponsor" src={SponsorForbes} />
+                  <img alt="fortune-sponsor" src={SponsorFortune} />
+                </div>
+              </div>
+            </div>
+          )}
+        </section>
+      </>
     );
 };
 
