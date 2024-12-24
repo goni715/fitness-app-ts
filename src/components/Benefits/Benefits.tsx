0c95ec88f47156235f8e7208f1e7b1f48a459769
goni715 <halesgoni715149123@gmail.com>
2024-12-24 20:53:15 +0600

    HText

--- src/components/Benefits/Benefits.tsx
+++ src/components/Benefits/Benefits.tsx
@@ -9,7 +9,19 @@ type TProps = {
 const Benefits = ({ setSelectedPage }: TProps) => {
     return (
         <>
-            <h1>Benefits component</h1>
+            <section id="benefits" className="mx-auto min-h-full w-5/6 py-20"> 
+            <div>
+                {/* Header */}
+                <div className="md:my-5 md:w-3/5">
+                <HText>MORE THAN JUST GYM.</HText>
+          <p className="my-5 text-sm">
+            We provide world class fitness equipment, trainers and classes to
+            get you to your ultimate fitness goals with ease. We provide true
+            care into each and every member.
+          </p>
+                </div>
+            </div>
+            </section>
         </>
     );
 };
