6b26ae8121c00914b0aeba809c75ac6a722d49ec
goni715 <halesgoni715149123@gmail.com>
2024-12-26 09:14:16 +0600

    ourclasses component

--- src/components/OurClasses/ClassItem.tsx
+++ src/components/OurClasses/ClassItem.tsx
@@ -1,16 +1,26 @@
-
 interface TProps {
-    name: string;
-    description?: string;
-    image: string;
-  }
+  name: string;
+  description?: string;
+  image: string;
+}
 
-const ClassItem = ({name, description, image} : TProps) => {
-    return (
-        <>
-            <h1>Class Item</h1>
-        </>
-    );
+const ClassItem = ({ name, description, image }: TProps) => {
+  const overlayStyles = `p-5 absolute z-30 flex
+  h-[380px] w-[450px] flex-col items-center justify-center
+  whitespace-normal bg-primary-500 text-center text-white
+  opacity-0 transition duration-500 hover:opacity-90`;
+
+  return (
+    <>
+      <li className="relative mx-5 inline-block h-[380px] w-[450px]">
+        <div className={overlayStyles}>
+          <p className="text-2xl">{name}</p>
+          <p className="mt-5">{description}</p>
+        </div>
+        <img alt={`${image}`} src={image} />
+      </li>
+    </>
+  );
 };
 
 export default ClassItem;
--- src/components/OurClasses/OurClasses.tsx
+++ src/components/OurClasses/OurClasses.tsx
@@ -3,6 +3,8 @@ import { TClassType, TSelectedPage } from "../../shared/types";
 import HText from "../../shared/HText";
 import { classes } from "../../data/data";
 import ClassItem from "./ClassItem";
+import { motion } from "framer-motion";
+
 
 type TProps = {
     setSelectedPage : Dispatch<SetStateAction<TSelectedPage>>
@@ -12,8 +14,20 @@ const OurClasses = ({ setSelectedPage }: TProps) => {
     return (
       <>
         <section id="ourclasses" className="w-full bg-primary-100 py-40">
-          <div>
-            <div className="mx-auto w-5/6">
+          <motion.div
+            onViewportEnter={() => setSelectedPage(TSelectedPage.OurClasses)}
+          >
+            <motion.div 
+              className="mx-auto w-5/6"
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.5 }}
+              transition={{ duration: 0.5 }}
+              variants={{
+                hidden: { opacity: 0, x: -50 },
+                visible: { opacity: 1, x: 0 },
+              }}
+            >
               <div className="md:w-3/5">
                 <HText>OUR CLASSES</HText>
                 <p className="py-5">
@@ -23,7 +37,7 @@ const OurClasses = ({ setSelectedPage }: TProps) => {
                   Rhoncus sit enim mattis odio in risus nunc.
                 </p>
               </div>
-            </div>
+            </motion.div>
             <div className="mt-10 h-[353px] w-full overflow-x-auto overflow-y-hidden">
               <ul className="w-[2800px] whitespace-nowrap">
                 {classes.map((item: TClassType, index) => (
@@ -36,7 +50,7 @@ const OurClasses = ({ setSelectedPage }: TProps) => {
                 ))}
               </ul>
             </div>
-          </div>
+          </motion.div>
         </section>
       </>
     );
